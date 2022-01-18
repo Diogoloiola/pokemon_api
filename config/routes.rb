1c0c5635b2e46f69454a099bbe1e175a20d86de0
Diogoloiola <diogomouraloiola@gmail.com>
2022-01-18 20:54:21 -0300

    update: file routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
-  namespace :v1 do
+  namespace :v1, defaults: { format: :json } do
     resources :pokemons
   end
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
